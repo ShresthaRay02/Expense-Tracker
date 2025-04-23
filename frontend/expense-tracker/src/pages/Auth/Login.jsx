cc6050c3c24f25dd597e358d50661061347880b4
ShresthaRay02 <shrestharay02@gmail.com>
2025-04-23 23:10:12 +0530

    Integrated the Login API

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from '../../components/layouts/AuthLayout'
 import Input from '../../components/Inputs/Input'
 import { validateEmail } from '../../utils/helper';
 import {Link, useNavigate} from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import {API_PATHS} from "../../utils/apiPath"
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -25,7 +27,27 @@ const handleLogin  = async (e) => {
     return;
   }
 
-  setError("");//Login API call
+  setError("");
+
+  //Login API call
+  try{
+    const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN,{
+      email,
+      password,
+    });
+    const{token, user}=response.data;
+
+    if(token){
+      localStorage.setItem("token",token);
+      navigate("/dashboard");
+    }
+  }catch(error){
+    if(error.response && error.response.data.message){
+      setError(error.response.data.message);
+    }else{
+      setError("Something went wrong. Please try again.");
+    }
+  }
 }
 
   return (
